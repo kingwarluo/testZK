0351fc9ef824ac78c5be0f4deaa89c96be102927
罗建华 <jianhua.luo@ucarinc.com>
2019-07-09 10:09:49 +0800

    布隆过滤器

--- src/main/java/com/bloomfilter/BloomFilterTest.java
+++ src/main/java/com/bloomfilter/BloomFilterTest.java
@@ -1,5 +1,47 @@
 package com.bloomfilter;
 
+import com.google.common.base.Charsets;
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnel;
+import com.google.common.hash.Funnels;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+import lombok.experimental.Builder;
+
+/**
+ * @author jianhua.luo
+ * @description:布隆过滤器测试
+ * @date 2019/7/8
+ */
 public class BloomFilterTest {
+
+    public static void main(String[] args) {
+        int expectedInsertions = 10000000;
+        double fpp = 0.00001;
+
+        BloomFilter<CharSequence> bloomFilter = BloomFilter.create(Funnels.stringFunnel(), expectedInsertions, fpp);
+
+        bloomFilter.put("aaa");
+        bloomFilter.put("bbb");
+        boolean containsString = bloomFilter.mightContain("aaa");
+        System.out.println(containsString);
+
+        BloomFilter<Email> emailBloomFilter = BloomFilter
+                .create((from, into) -> into.putString(from.getDomain(), Charsets.UTF_8),
+                        expectedInsertions, fpp);
+
+        emailBloomFilter.put(new Email("sage.wang", "quanr.com"));
+        boolean containsEmail = emailBloomFilter.mightContain(new Email("sage.wangaaa", "quanr.com"));
+        System.out.println(containsEmail);
+    }
+
+    @Data
+    @ToString
+    @AllArgsConstructor
+    public static class Email {
+        private String userName;
+        private String domain;
+    }
 
 }
