526935023f305208cec054bc430da1954846ccf6
罗建华 <jianhua.luo@ucarinc.com>
2020-05-19 15:46:17 +0800

    zookeeper分布式锁原理说明

--- src/main/java/com/zookeeper/ZooKeeperDistributedLock.java
+++ src/main/java/com/zookeeper/ZooKeeperDistributedLock.java
@@ -69,6 +69,12 @@ public class ZooKeeperDistributedLock implements Watcher {
             // 假设productId代表了一个商品id，比如说1
             // locksRoot = locks
             // /locks/10000000000，/locks/10000000001，/locks/10000000002
+            //  自己补充：1.Zookeeper的每一个节点，都是一个天然的顺序发号器。
+            // 在每一个节点下面创建子节点时，只要选择的创建类型是有序（EPHEMERAL_SEQUENTIAL 临时有序或者PERSISTENT_SEQUENTIAL 永久有序）类型，
+            // 那么，新的子节点后面，会加上一个次序编号。这个次序编号，是上一个生成的次序编号加一
+            //比如，创建一个用于发号的节点“/test/lock”，然后以他为父亲节点，可以在这个父节点下面创建相同前缀的子节点，假定相同的前缀为“/test/lock/seq-”，
+            // 在创建子节点时，同时指明是有序类型。如果是第一个创建的子节点，那么生成的子节点为/test/lock/seq-0000000000，下一个节点则为/test/lock/seq-0000000001，
+            // 依次类推，等等。
             lockNode = zk.create(locksRoot + "/" + productId, new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
 
             // 看看刚创建的节点是不是最小的节点
@@ -76,6 +82,7 @@ public class ZooKeeperDistributedLock implements Watcher {
             List<String> locks = zk.getChildren(locksRoot, false);
             Collections.sort(locks);
 
+            //自己补充：2.Zookeeper节点的递增性，可以规定节点编号最小的那个获得锁。
             if (lockNode.equals(locksRoot + "/" + locks.get(0))) {
                 //如果是最小的节点,则表示取得锁
                 return true;
@@ -99,6 +106,14 @@ public class ZooKeeperDistributedLock implements Watcher {
         return false;
     }
 
+    // 自己补充：3.Zookeeper的节点监听机制，可以保障占有锁的方式有序而且高效。
+    // 每个线程抢占锁之前，先抢号创建自己的ZNode。同样，释放锁的时候，就需要删除抢号的Znode。抢号成功后，如果不是排号最小的节点，就处于等待通知的状态。等谁的通知呢？不需要其他人，只需要等前一个Znode 的通知就可以了。当前一个Znode 删除的时候，就是轮到了自己占有锁的时候。第一个通知第二个、第二个通知第三个，击鼓传花似的依次向后。
+    // Zookeeper的节点监听机制，可以说能够非常完美的，实现这种击鼓传花似的信息传递。具体的方法是，每一个等通知的Znode节点，只需要监听linsten或者 watch 监视排号在自己前面那个，而且紧挨在自己前面的那个节点。 只要上一个节点被删除了，就进行再一次判断，看看自己是不是序号最小的那个节点，如果是，则获得锁。
+
+    // 在创建取号节点的时候，尽量创建临时znode 节点而不是永久znode 节点，一旦这个 znode 的客户端与Zookeeper集群服务器失去联系，这个临时 znode 也将自动删除。
+
+    //Zookeeper这种首尾相接，后面监听前面的方式，可以避免羊群效应。所谓羊群效应就是每个节点挂掉，所有节点都去监听，然后做出反映，这样会给服务器带来巨大压力，
+    // 所以有了临时顺序节点，当一个节点挂掉，只有它后面的那一个节点才做出反映。
     private boolean waitForLock(String waitNode, long waitTime) throws InterruptedException, KeeperException {
         Stat stat = zk.exists(locksRoot + "/" + waitNode, true);
         if (stat != null) {
