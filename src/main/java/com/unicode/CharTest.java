82f17b3d3697d46bcf2e66f67f35f48335639a89
罗建华 <jianhua.luo@ucarinc.com>
2020-12-23 00:06:24 +0800

    char理解

--- src/main/java/com/unicode/CharTest.java
+++ src/main/java/com/unicode/CharTest.java
@@ -1,18 +1,41 @@
 package com.unicode;
 
 import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
 
 public class CharTest {
 
     public static void main(String[] args) {
         String name = "I'm 华哥.";
         toHex(name);
-
+        // 转换成16进制编码
         toHex(name, "ISO-8859-1");
         toHex(name, "GB2312");
         toHex(name, "GBK");
         toHex(name, "UTF-16");
         toHex(name, "UTF-8");
+
+        System.out.println("==================================");
+
+        // 转换成char数组，用不同编码方式编码
+        encode(name, "ISO-8859-1");
+        encode(name, "GB2312");
+        encode(name, "GBK");
+        encode(name, "UTF-16");
+        encode(name, "UTF-8");
+    }
+
+    private static String encode(String str, String charsetName) {
+        try {
+            String newStr = new String(str.getBytes(), charsetName);
+            System.out.println(str + " use " + charsetName + " to encode is: " + newStr);
+            return newStr;
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     private static String toHex(String str, String charsetName) {
@@ -20,7 +43,7 @@ public class CharTest {
             byte[] bytes = str.getBytes(charsetName);
             StringBuilder sb = new StringBuilder();
             for (byte b : bytes) {
-                sb.append(String.format("%02x", b));
+                sb.append(String.format("%02x", b)).append(" ");
             }
             System.out.println(str + " use " + charsetName + " to hex is: " + sb.toString());
             return sb.toString();
