99241935d03d443fcdad0e4ec9bb88169f20a7b6
kingwarluo <jianhua.luo@ucarinc.com>
2020-12-24 15:00:57 +0800

    添加是否包含emoji判断

--- src/main/java/com/unicode/UnicodeTest.java
+++ src/main/java/com/unicode/UnicodeTest.java
@@ -19,6 +19,8 @@ public class UnicodeTest {
         String nameSuffix = subString(content, realStringLength - 1, 1);
         content = String.format("%s%s%s", namePrefix, "*", nameSuffix);
         System.out.println(content);
+
+        System.out.println(containsEmoji(content));
     }
 
     /**
@@ -70,6 +72,48 @@ public class UnicodeTest {
             count++;
         }
         return count;
+    }
+
+    /**
+     * 是否包含emoji表情
+     *
+     * @author jianhua.luo
+     * @date 2020/12/24
+     */
+    public static boolean containsEmoji(String source) {
+        int len = source.length();
+        for (int i = 0; i < len; i++) {
+            char hs = source.charAt(i);
+            if (0xd800 <= hs && hs <= 0xdbff) {
+                if (source.length() > 1) {
+                    char ls = source.charAt(i+1);
+                    int uc = ((hs - 0xd800) * 0x400) + (ls - 0xdc00) + 0x10000;
+                    if (0x1d000 <= uc && uc <= 0x1f77f) {
+                        return true;
+                    }
+                }
+            } else {
+                // non surrogate
+                if (0x2100 <= hs && hs <= 0x27ff && hs != 0x263b) {
+                    return true;
+                } else if (0x2B05 <= hs && hs <= 0x2b07) {
+                    return true;
+                } else if (0x2934 <= hs && hs <= 0x2935) {
+                    return true;
+                } else if (0x3297 <= hs && hs <= 0x3299) {
+                    return true;
+                } else if (hs == 0xa9 || hs == 0xae || hs == 0x303d || hs == 0x3030 || hs == 0x2b55 || hs == 0x2b1c || hs == 0x2b1b || hs == 0x2b50 ) {
+                    return true;
+                }
+                if (source.length() > 1 && i < source.length() -1) {
+                    char ls = source.charAt(i+1);
+                    if (ls == 0x20e3) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return  false;
     }
 
 }
