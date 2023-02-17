b0f11c505610a82341d595f32c28709af1cbcc34
luojianhua <kingwarluo@gmail.com>
2023-02-17 18:51:35 +0800

    asm字节码学习
    Signed-off-by: luojianhua <kingwarluo@gmail.com>

--- src/main/java/com/asm/HelloWorld.java
+++ src/main/java/com/asm/HelloWorld.java
@@ -11,7 +11,7 @@ import org.objectweb.asm.Opcodes;
  * the hello world byte code generate 
  * by asm.
  * @author Administrator
- *
+ * 参考：https://blog.51cto.com/memory/1035551
  */
 public class HelloWorld {
     
@@ -20,38 +20,39 @@ public class HelloWorld {
         new SecurityChecker().getA();
 
         ClassWriter classWriter = new ClassWriter(0);
-        String className = "com/asm/HelloWorld";
-        classWriter.visit(Opcodes.V1_5, Opcodes.ACC_PUBLIC, className, null, 
+        String className = "HelloWorld";
+        classWriter.visit(Opcodes.V1_5, Opcodes.ACC_PUBLIC, className, null,
                                           "java/lang/Object", null);
 
         MethodVisitor initVisitor = classWriter.visitMethod(Opcodes.ACC_PUBLIC, "<init>",
                                            "()V", null, null);
         initVisitor.visitCode();//访问开始
         initVisitor.visitVarInsn(Opcodes.ALOAD, 0);//this指针入栈
-        initVisitor.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/Object", "<init>", 
-                                            "V()");//调用构造函数
+        initVisitor.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/Object", "<init>",
+                                            "()V");//调用构造函数
         initVisitor.visitInsn(Opcodes.RETURN);
         initVisitor.visitMaxs(1, 1);//设置栈长、本地变量数
         initVisitor.visitEnd();//访问结束
-        
-        MethodVisitor helloVisitor = classWriter.visitMethod(Opcodes.ACC_PUBLIC, "sayHello", 
-                                           "()V;", null, null);
-        helloVisitor.visitCode();
-        helloVisitor.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", 
+
+        MethodVisitor mainVisitor = classWriter.visitMethod(Opcodes.ACC_PUBLIC | Opcodes.ACC_STATIC, "main",
+                "([Ljava/lang/String;)V", null, null);
+        mainVisitor.visitCode();
+        mainVisitor.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out",
                                              "Ljava/io/PrintStream;");
-        helloVisitor.visitLdcInsn("hello world!");
-        helloVisitor.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", 
+        mainVisitor.visitLdcInsn("hello world!");
+        mainVisitor.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream",
                                                 "println", "(Ljava/lang/String;)V");
-        helloVisitor.visitInsn(Opcodes.RETURN);
-        helloVisitor.visitMaxs(1, 1);
-        helloVisitor.visitEnd();
+        mainVisitor.visitInsn(Opcodes.RETURN);
+        mainVisitor.visitMaxs(0, 0);
+        mainVisitor.visitEnd();
         
         classWriter.visitEnd();
         byte[] code = classWriter.toByteArray();
-        File file = new File("D:\\HelloWorld.class");
-        FileOutputStream output = new FileOutputStream(file);
-        output.write(code);
-        output.close();
+        // 输出到本地文件
+//        File file = new File("D:\\com\\HelloWorld.class");
+//        FileOutputStream output = new FileOutputStream(file);
+//        output.write(code);
+//        output.close();
     }
 
 }
