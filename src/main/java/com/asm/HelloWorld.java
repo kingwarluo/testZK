361fad9c3b66577b86ffb1c14b6e2382c145adf2
luojianhua <kingwarluo@gmail.com>
2023-02-18 16:14:08 +0800

    asm字节码学习--调用本地静态方法
    Signed-off-by: luojianhua <kingwarluo@gmail.com>

--- src/main/java/com/asm/HelloWorld.java
+++ src/main/java/com/asm/HelloWorld.java
@@ -2,6 +2,7 @@ package com.asm;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.lang.reflect.Method;
 
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.MethodVisitor;
@@ -19,8 +20,8 @@ public class HelloWorld {
 
         new SecurityChecker().getA();
 
-        ClassWriter classWriter = new ClassWriter(0);
-        String className = "HelloWorld";
+        ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+        String className = "com/HelloWorld";
         classWriter.visit(Opcodes.V1_5, Opcodes.ACC_PUBLIC, className, null,
                                           "java/lang/Object", null);
 
@@ -34,6 +35,19 @@ public class HelloWorld {
         initVisitor.visitMaxs(1, 1);//设置栈长、本地变量数
         initVisitor.visitEnd();//访问结束
 
+        // 添加sayHello方法
+        MethodVisitor sayVisitor = classWriter.visitMethod(Opcodes.ACC_PUBLIC | Opcodes.ACC_STATIC, "sayHello",
+                "()V", null, null);
+        sayVisitor.visitCode();
+        sayVisitor.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out",
+                "Ljava/io/PrintStream;");
+        sayVisitor.visitLdcInsn("h!e!l!l!o!");
+        sayVisitor.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream",
+                "println", "(Ljava/lang/String;)V");
+        sayVisitor.visitInsn(Opcodes.RETURN);
+        sayVisitor.visitMaxs(0, 0);
+        sayVisitor.visitEnd();
+
         MethodVisitor mainVisitor = classWriter.visitMethod(Opcodes.ACC_PUBLIC | Opcodes.ACC_STATIC, "main",
                 "([Ljava/lang/String;)V", null, null);
         mainVisitor.visitCode();
@@ -42,17 +56,32 @@ public class HelloWorld {
         mainVisitor.visitLdcInsn("hello world!");
         mainVisitor.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream",
                                                 "println", "(Ljava/lang/String;)V");
+        mainVisitor.visitMethodInsn(Opcodes.INVOKESTATIC, "com/HelloWorld",
+                "sayHello", "()V");
         mainVisitor.visitInsn(Opcodes.RETURN);
         mainVisitor.visitMaxs(0, 0);
         mainVisitor.visitEnd();
         
         classWriter.visitEnd();
         byte[] code = classWriter.toByteArray();
-        // 输出到本地文件
+//        // 输出到本地文件
 //        File file = new File("D:\\com\\HelloWorld.class");
 //        FileOutputStream output = new FileOutputStream(file);
 //        output.write(code);
 //        output.close();
+
+        Class clazz = new ClassLoader() {
+            @Override
+            protected Class findClass(String name)
+                throws ClassNotFoundException {
+                return defineClass(name, code, 0, code.length);
+            }
+        }.loadClass("com.HelloWorld");
+
+        // 利用反射获取com.HelloWorld类中的main方法 
+        Method method = clazz.getMethod("main", new Class[]{String[].class});
+        // 用反射的方式调用com.HelloWorld类中的main() 
+        method.invoke(null, (Object) new String[0]);
     }
 
 }
